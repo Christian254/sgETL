aab1036101d6454a76837fbc7efc3b55d3555e46
Zoila Yaneth <yanethvillatoro444@yahoo.com>
2019-06-14 19:12:11 -0600

    Entradas Reportes Tacticos terminado

--- sgCibercachadas/sgCibercachadas/tactico/urls.py
+++ sgCibercachadas/sgCibercachadas/tactico/urls.py
@@ -2,7 +2,10 @@ from django.urls import include, path
 from tactico.views import *
 
 urlpatterns = [
-    path('productosmasvendidos',ProductosMasVendidosView.as_view(),name='re_producto_mas_vendidos'),
-    path('productosgeneranganancia',ProductosGeneranGananciaView.as_view(),name='re_producto_generan_ganancia'),
-
+    path('productosmasvendidos',ProductosMasVendidosView.as_view(),name='semire_producto_mas_vendidos'),
+    path('productosgeneranganancia',ProductosGeneranGananciaView.as_view(),name='semire_producto_generan_ganancia'),
+    path('retornoequiposgarantia',RetornoEquiposGarantiaView.as_view(),name='semire_retorno_equipos_garantia'),
+    path('productosenconsigna',RetornoEnConsignaView.as_view(),name='semire_productos_consigna'),
+    path('clientesganancia',ClientesGananciaView.as_view(),name='semire_clientes_ganancia'),
+    path('clientesfrecuentes',ClientesFrecuentesView.as_view(),name='semire_clientes_frecuentes'),
 ]
--- sgCibercachadas/sgCibercachadas/tactico/views.py
+++ sgCibercachadas/sgCibercachadas/tactico/views.py
@@ -7,3 +7,15 @@ class ProductosMasVendidosView(generic.TemplateView):
 
 class ProductosGeneranGananciaView(generic.TemplateView):
     template_name='tactico/productos_generan_ganancias.html'
+
+class RetornoEquiposGarantiaView(generic.TemplateView):
+    template_name='tactico/retorno_equipos_garantia.html'
+
+class RetornoEnConsignaView(generic.TemplateView):
+    template_name='tactico/productos_consigna.html'
+
+class ClientesGananciaView(generic.TemplateView):
+    template_name='tactico/ganancia_clientes.html'
+
+class ClientesFrecuentesView(generic.TemplateView):
+    template_name='tactico/clientes_frecuentes.html'
