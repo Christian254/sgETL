d24c2c6b433ae5126aa60ff70edc2986613cf5f8
Christian254 <christianfuentes254@gmail.com>
2019-06-14 22:56:33 -0600

    validaciones
    faltaria validar las categorias en algunos casos

--- sgCibercachadas/sgCibercachadas/estrategico/views.py
+++ sgCibercachadas/sgCibercachadas/estrategico/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.views import generic
 from general.reporte import plantilla_reporte
 from django.contrib import messages
+from datetime import datetime
 # Create your views here.
 
 class ProductosGeneranGananciasView(generic.TemplateView):
@@ -9,21 +10,77 @@ class ProductosGeneranGananciasView(generic.TemplateView):
     def post(self, request, *args, **kwargs):
         inicio = request.POST.get('fechainicio',None)
         fin = request.POST.get('fechafin',None)
-        if(inicio==None or fin==None):
+        if(not(inicio) or not(fin)):
             messages.add_message(request, messages.WARNING, 'Las fechas son obligatorias')
             return redirect(self.request.path_info)
+
+        elif(str(datetime.strptime(inicio,'%d/%m/%Y')) > str(datetime.strptime(fin,'%d/%m/%Y'))):
+            messages.add_message(request, messages.WARNING, 'Las fechas de inicio debe ser menor que la fecha de fin')
+            return redirect(self.request.path_info)
         else:
-            nota = []
+            nota =[]
             return plantilla_reporte(request,nota,'prueba')
 
 class ProductosPotencialesView(generic.TemplateView):
     template_name='estrategico/productos_potenciales.html'
+    def post(self, request, *args, **kwargs):
+        inicio = request.POST.get('fechainicio',None)
+        fin = request.POST.get('fechafin',None)
+        if(not(inicio) or not(fin)):
+            messages.add_message(request, messages.WARNING, 'Las fechas son obligatorias')
+            return redirect(self.request.path_info)
+
+        elif(str(datetime.strptime(inicio,'%d/%m/%Y')) > str(datetime.strptime(fin,'%d/%m/%Y'))):
+            messages.add_message(request, messages.WARNING, 'Las fechas de inicio debe ser menor que la fecha de fin')
+            return redirect(self.request.path_info)
+        else:
+            nota =[]
+            return plantilla_reporte(request,nota,'prueba')
 
 class ProductosGananciasClientesView(generic.TemplateView):
     template_name='estrategico/productos_ganancias_clientes.html'
+    def post(self, request, *args, **kwargs):
+        inicio = request.POST.get('fechainicio',None)
+        fin = request.POST.get('fechafin',None)
+        if(not(inicio) or not(fin)):
+            messages.add_message(request, messages.WARNING, 'Las fechas son obligatorias')
+            return redirect(self.request.path_info)
+
+        elif(str(datetime.strptime(inicio,'%d/%m/%Y')) > str(datetime.strptime(fin,'%d/%m/%Y'))):
+            messages.add_message(request, messages.WARNING, 'Las fechas de inicio debe ser menor que la fecha de fin')
+            return redirect(self.request.path_info)
+        else:
+            nota =[]
+            return plantilla_reporte(request,nota,'prueba')
 
 class ProductosVendidosView(generic.TemplateView):
     template_name='estrategico/productos_vendidos.html'
+    def post(self, request, *args, **kwargs):
+        inicio = request.POST.get('fechainicio',None)
+        fin = request.POST.get('fechafin',None)
+        if(not(inicio) or not(fin)):
+            messages.add_message(request, messages.WARNING, 'Las fechas son obligatorias')
+            return redirect(self.request.path_info)
+
+        elif(str(datetime.strptime(inicio,'%d/%m/%Y')) > str(datetime.strptime(fin,'%d/%m/%Y'))):
+            messages.add_message(request, messages.WARNING, 'Las fecha de inicio debe ser menor que la fecha de fin')
+            return redirect(self.request.path_info)
+        else:
+            nota =[]
+            return plantilla_reporte(request,nota,'prueba')
 
 class ProductosTardanzaProductosView(generic.TemplateView):
     template_name='estrategico/productos_tardanzas_movimiento.html'
+    def post(self, request, *args, **kwargs):
+        inicio = request.POST.get('fechainicio',None)
+        fin = request.POST.get('fechafin',None)
+        if(not(inicio) or not(fin)):
+            messages.add_message(request, messages.WARNING, 'Las fechas son obligatorias')
+            return redirect(self.request.path_info)
+
+        elif(str(datetime.strptime(inicio,'%d/%m/%Y')) > str(datetime.strptime(fin,'%d/%m/%Y'))):
+            messages.add_message(request, messages.WARNING, 'Las fechas de inicio debe ser menor que la fecha de fin')
+            return redirect(self.request.path_info)
+        else:
+            nota =[]
+            return plantilla_reporte(request,nota,'prueba')
