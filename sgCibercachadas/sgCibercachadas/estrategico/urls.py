5ccb7ff7f6b95c825696eb577b791f5dbd84177c
Christian254 <christianfuentes254@gmail.com>
2019-06-14 20:02:42 -0600

    corrección
    con vista basada en clase

--- sgCibercachadas/sgCibercachadas/estrategico/urls.py
+++ sgCibercachadas/sgCibercachadas/estrategico/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from estrategico.views import *
 
 urlpatterns = [
-    path('productosgeneranganancias',ProductosGeneranGananciasView,name='re_producto_generan_ganancias'),
+    path('productosgeneranganancias',ProductosGeneranGananciasView.as_view(),name='re_producto_generan_ganancias'),
     path('productospotenciales',ProductosPotencialesView.as_view(),name='re_producto_potenciales'),
     path('productosgananciasclientes',ProductosGananciasClientesView.as_view(),name='re_producto_ganancias_clientes'),
     path('productosmasvendidos',ProductosVendidosView.as_view(),name='re_producto_vendido'),
--- sgCibercachadas/sgCibercachadas/estrategico/views.py
+++ sgCibercachadas/sgCibercachadas/estrategico/views.py
@@ -1,13 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views import generic
 from general.reporte import plantilla_reporte
+from django.contrib import messages
 # Create your views here.
 
-def ProductosGeneranGananciasView(request):
-    if request.method == 'POST':
-        nota = []
-        return plantilla_reporte(request, nota, 'prueba')
-    return render(request, 'estrategico/productos_mas_ganancias.html',{})
+class ProductosGeneranGananciasView(generic.TemplateView):
+    template_name='estrategico/productos_mas_ganancias.html'
+    def post(self, request, *args, **kwargs):
+        inicio = request.POST.get('fechainicio',None)
+        fin = request.POST.get('fechafin',None)
+        if(inicio==None or fin==None):
+            messages.add_message(request, messages.WARNING, 'Las fechas son obligatorias')
+            return redirect(self.request.path_info)
+        else:
+            nota = []
+            return plantilla_reporte(request,nota,'prueba')
 
 class ProductosPotencialesView(generic.TemplateView):
     template_name='estrategico/productos_potenciales.html'
