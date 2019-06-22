6170e630aab3c5ea2ab8841e2877e4d8f69c7f3d
Zoila Yaneth <yanethvillatoro444@yahoo.com>
2019-06-22 17:16:28 -0600

    Crear Usuarios

--- sgCibercachadas/sgCibercachadas/administrador/urls.py
+++ sgCibercachadas/sgCibercachadas/administrador/urls.py
@@ -3,5 +3,5 @@ from administrador.views import *
 
 urlpatterns = [
     path('adminusuarios',AdminUsuariosView.as_view(),name='admin_gestion_usuarios'),
-  
+    path('crearusuarios',CrearUsuariosView.as_view(),name='admin_crear_usuarios'),
 ]
--- sgCibercachadas/sgCibercachadas/administrador/views.py
+++ sgCibercachadas/sgCibercachadas/administrador/views.py
@@ -2,10 +2,41 @@ from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixi
 from django.views import generic
 from django.views.generic.list import ListView
 from django.contrib.auth.models import User
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import Group
+from django.contrib import messages
+
+from administrador.forms import *
 
 class AdminUsuariosView(generic.ListView):
     model = User
     template_name='administrador/admin_usuarios.html'
     context_object_name = "obj"
-    paginate_by = 10
+    paginate_by = 8
 
+class CrearUsuariosView(generic.TemplateView):
+    template_name='administrador/crear_usuarios.html'
+    
+    def get(self,request,*args,**kwargs):
+        form= UsuarioForm()
+        return render(request,self.template_name,{"form":form})    
+    def post(self,request,*args,**kwargs):   
+        form= UsuarioForm(request.POST)
+        if form.is_valid():
+            my_group = Group.objects.get(id=request.POST.get('rol')) 
+            user = User.objects.create_user(
+                                username=request.POST.get('username'),
+                                first_name=request.POST.get('nombres'),
+                                last_name=request.POST.get('apellidos'),
+                                email=request.POST.get('email'),
+                                password=request.POST.get('password'))
+            my_group.user_set.add(user)
+            messages.add_message(request, messages.SUCCESS, 'Ingreso del Usuario con Username: '+request.POST.get('username')+' Contraseña: '+request.POST.get('password'))
+
+            return redirect(self.request.path_info)
+        else:
+            form.AddIsInvalid()
+            return render(request,self.template_name,{"form":form})
